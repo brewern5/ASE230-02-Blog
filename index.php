cb0c2f73a66844946ae8d304e231c85527bfa718
Nate Brewer <119635918+brewern5@users.noreply.github.com>
2024-09-17 12:38:59 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,32 @@
 <?php
 
 $blogdata = [
-    ['tile' => 'Title 1', 'content' => 'this is the content for title 1', 'author' => 'Danny Poff', 'date' => 'Today'],
-    ['tile' => 'Title 2', 'content' => 'this is the content for title 2', 'author' => 'Nate Brewer', 'date' => 'Today Aswell']
+    ['title' => 'Title 1', 'content' => 'this is the content for title 1', 'author' => 'Danny Poff', 'date' => 'Today'],
+    ['title' => 'Title 2', 'content' => 'this is the content for title 2', 'author' => 'Nate Brewer', 'date' => 'Today Aswell']
 ];
 
+function displayElement($element,$x) { ?>
+    <header class="resume-header pt-4 pt-md-0">
+        <div class="row">
+            <div class="col">
+                <div class="row p-4 justify-content-center justify-content-md-between">
+                    <div class="primary-info col-auto">
+                        <a href = "detail.php?x=<?php echo $x;?>"><?=$element['title']; ?></a>
+                    </div><!--//primary-info-->
+                </div><!--//row-->
+            
+            </div><!--//col-->
+        </div><!--//row-->
+    </header>
 
+
+<?php
+}
 
 ?>
+<!DOCTYPE html>
+<html>
+
+    <?php for($x=0;$x<count($blogdata);$x++) displayElement($blogdata[$x],$x); ?>
+
+</html>
