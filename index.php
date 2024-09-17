f6a443ba7be8d574d56dcb88cda00f79fd9a1762
Nate Brewer <119635918+brewern5@users.noreply.github.com>
2024-09-17 12:10:00 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+$blogdata = [
+    ['tile' => 'Title 1', 'content' => 'this is the content for title 1', 'author' => 'Danny Poff', 'date' => 'Today'],
+    ['tile' => 'Title 2', 'content' => 'this is the content for title 2', 'author' => 'Nate Brewer', 'date' => 'Today Aswell']
+];
+
+
+
+?>
